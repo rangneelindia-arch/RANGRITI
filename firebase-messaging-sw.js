34ee2f2f0ab1465c6e95c3e67fd0c7d9d30c9345
rangneelindia-arch <rangneelindia@gmail.com>
2026-07-29 10:37:36 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,28 @@
+// firebase-messaging-sw.js
+
+// Firebase लाइब्रेरीज लोड करें (वर्जन 8.10.1 होना चाहिए)
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// अपना वही Firebase Config यहाँ डालें
+// अपना Firebase Config डालें
 const firebaseConfig = {
   apiKey: "AIzaSyAuK0KSBqksepQ_ZhpNxPUEwGsX051pICU",
   authDomain: "rangritiapp.firebaseapp.com",
+  databaseURL: "https://rangritiapp-default-rtdb.firebaseio.com",
   projectId: "rangritiapp",
   storageBucket: "rangritiapp.firebasestorage.app",
   messagingSenderId: "344639418240",
   appId: "1:344639418240:web:e8fb70f868bfbc9798db65"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// जब ऐप बंद हो, तब बैकग्राउंड में मैसेज रिसीव करने का कोड
+// जब ऐप बैकग्राउंड में हो, तब नोटिफिकेशन दिखाने का कोड
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
